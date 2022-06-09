6245b4e0ada86ca9ff383845eaa3e8115e094f05
TheOnly05 <79068047+TheOnly05@users.noreply.github.com>
2022-06-09 23:53:47 +0200

    Update EnemigoMovement.cs

--- EnemigoMovement.cs
+++ EnemigoMovement.cs
@@ -3,18 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemigoMovement : MonoBehaviour
-{
-    //float gravedad = -9.8f;
-     
-    //void Start()
-    //{
-         
-    //}
+{ 
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    //eneRig.velocity = new(eneVel, eneRig.velocity.x) ;
-    //    transform.Translate(0, 0, 2f * Time.deltaTime);
-    //}
 }
